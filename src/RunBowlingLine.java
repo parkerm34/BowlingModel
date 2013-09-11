ed4797d485c4f805b9163beb194b70e2203afe61
Parker Mathewson <parkerfm34@gmail.com>
2013-09-11 14:55:17 -0700

    first commit

---
+++ src/RunBowlingLine.java
@@ -0,0 +1,13 @@
+
+public class RunBowlingLine {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println("Hi");
+		BowlingLine game1 = new BowlingLine();
+	}
+
+}
