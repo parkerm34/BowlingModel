83a00f4352d6e5a3453589a1624eb55ba63bcda1
Parker Mathewson <parkerfm34@gmail.com>
2013-09-12 01:36:37 -0700

    comments for Parkers code

--- src/BowlingLine.java
+++ src/BowlingLine.java
@@ -1,3 +1,68 @@
+/*
+ * Parker Mathewson and Nicholas Pierson
+ * 
+ * NetID: Parkerm34 and nopierson
+ * 
+ * University of Arizona, CSc 335, Mercer Fall '13
+ * 
+ * 9/11/13
+ * 
+ * File: BowlingLine.java
+ * 
+ * 
+ * Functions
+ * +++++++++
+ * Public
+ * ======
+ * BowlingLine()
+ * int getPins()
+ * int getCurrentFrame()
+ * int getRollsThisFrame()
+ * int getScore()
+ * void setPins(int remain)
+ * void setCurrentFrame(int frame)
+ * void setRollsThisFrame(int bowls)
+ * 
+ * Private
+ * =======
+ * void rollBall()
+ * void rollAction()
+ * void score(int downed)
+ * boolean bowlCheck(int downed)
+ * void enterRoll()
+ * 
+ * 
+ * Instance Variables
+ * ++++++++++++++++++
+ * Public
+ * ======
+ * 
+ * Private
+ * =======
+ * Scanner rolls
+ * Scanner bowl
+ * int pinsLeft
+ * int hit
+ * int rollsThisFrame
+ * int currentFrame
+ * int score
+ * 
+ * String invalidMessage = "Invalid roll, try again"
+ * 
+ * ---------------------------------------------------------------
+ * 
+ * This file is the main brain of the Bowling Model.
+ * This is where the methods will be kept that calculate the score during the whole game.
+ * Keeping score consists of which frame the bowler is in, what their total score is,
+ * which ball they are on in the frame, how many pins they have left in the current frame,
+ * and counts the strikes and spares in their legal ways in bowling rules. 
+ * 
+ * X represents a strike
+ * / represents a spare
+ * - represents a gutter ball
+ * 
+ */
+
 import java.util.Scanner;
 
 
@@ -14,7 +79,17 @@ public class BowlingLine {
 	
 	private static final String invalidMessage = "Invalid roll, try again";
 	
-	
+	/* Constructor for BowlingLine
+	 * Inputs: 
+	 * Outputs: game ending score.
+	 * 
+	 * What: This method initiates the whole game. It initially sets the score to 0
+	 * and sets the current frame to 1. Then, as if the machines came down, 10 pins
+	 * are sat nicely on the lane. The player bowls, the computer counts the pins
+	 * knocked down and compares them to make sure they arent bowling more pins
+	 * then there are standing. There is an error message if players are trying to
+	 * cheat. The bowl then gets scored and passed on to the rest of the game.
+	 */
 	public BowlingLine()
 	{
 		score = 0;
@@ -29,9 +104,16 @@ public class BowlingLine {
 		}
 		else
 			score(this.hit);
-			
 	}
 	
+	/* Wind up to the bowl
+	 * Inputs:
+	 * Outputs: 
+	 * 
+	 * What: This method is the turn repeater. This will make sure the game goes
+	 * for 10 frames. This is the same as the walk up or wind up in bowling. This
+	 * method then calls for the rollAction method
+	 */
 	private void rollBall()
 	{
 		while(currentFrame <= 10)
@@ -42,9 +124,17 @@ public class BowlingLine {
 			if(currentFrame < 10)
 				rollAction();
 		}
-		
 	}
 	
+	/* Action of throwing the ball
+	 * Inputs:
+	 * Outputs:
+	 * 
+	 * What: This method is the throwing of the ball, first, it asks the user
+	 * for input on what they rolled and checks to how many pins are left. 
+	 * Once a correct roll has happened, the knocked over pins will get added
+	 * to the score instance variable in the correct manner.
+	 */
 	private void rollAction()
 	{
 		enterRoll();
@@ -58,6 +148,21 @@ public class BowlingLine {
 			score(this.hit);
 	}
 	
+	/* Computer updating action
+	 * Input: pins hit over by the bowl
+	 * Output:
+	 * 
+	 * What: This method is what the computer will be calculating while
+	 * the ball is respawning. First, the method will take the number of
+	 * total pins and subtract the number hit over from the remaining.
+	 * The method will check to see which bowl in the frame it was, and
+	 * score accordingly:
+	 * 1st bowl of any frame, hitting over 10 is a strike
+	 * 2nd bowl of any frame, hitting over a total of 10 is a spare
+	 * 
+	 * A strike adds the next 2 bowls to the frame where a strike was achieved
+	 * A spare adds the next bowl to the fram where a spare was achieved
+	 */
 	private void score(int downed)
 	{
 		pinsLeft = pinsLeft - downed;
@@ -82,6 +187,9 @@ public class BowlingLine {
 		}
 	}
 	
+	/* Quick method in which a check for "knocking over more pins
+	 * than are up." Gotta prevent them cheaters!
+	 */
 	private boolean bowlCheck(int downed)
 	{
 		if((this.pinsLeft - downed) < 0)
@@ -89,12 +197,15 @@ public class BowlingLine {
 		return false;
 	}
 	
+	/* Quicker command for s.o.p("enter roll: ")
+	 */
 	private void enterRoll()
 	{
-		System.out.print("Enter roll: ");
-		
+		System.out.print("Enter roll: ");	
 	}
-	
+	/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+	/*                                 Setters and Getters                                         */
+	/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 	public void setPins(int remain)
 	{
 		this.pinsLeft = remain;
