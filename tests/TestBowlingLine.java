27c0f6fb0aa04efe1af1ff8fffb51784e1a538ce
Parker Mathewson <parkerfm34@gmail.com>
2013-09-11 23:21:21 -0700

    update 9-11-1121

--- tests/TestBowlingLine.java
+++ tests/TestBowlingLine.java
@@ -1,4 +1,26 @@
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+
+import org.junit.Test;
+
 
 public class TestBowlingLine {
+	
+	private BowlingLine game1 = new BowlingLine();
+	
+	private ByteArrayInputStream in = new ByteArrayInputStream("3\n7\n".getBytes());
+	
 
+	@Test
+	public void testStartGame()
+	{
+		assertEquals(game1.getPins(), 7);
+	}
+	
+	@Test
+	public void testGetters()
+	{
+		
+	}
 }
