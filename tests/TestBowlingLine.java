02e5c2a0e024592674829ad33e0116d6ee8ac3bd
nopierson <nopierson@email.arizona.edu>
2013-09-13 11:15:19 -0700

    More Testing; Inputed scores for entire game

--- tests/TestBowlingLine.java
+++ tests/TestBowlingLine.java
@@ -111,6 +111,8 @@ public class TestBowlingLine {
 		assertEquals(game1.getScoreboard()[2][19], '3');
 	}
 	
+	
+	
 	@Test
 	public void testSetLastFrameBowl()
 	{
@@ -141,12 +143,124 @@ public class TestBowlingLine {
 		game1.setLastFrameThirdBowl(3, 2);
 		assertEquals(game1.getScoreboard()[2][19], '3');
 	}
-	
+
 	@Test
 	public void testScoreboardTotal()
 	{
 		game1.setScore(53);
 		game1.setCurrentFrame(3);
-		assertEquals(game1.getScoreboard()[3][18], '5');
+		assertNotSame(game1.getScoreboard()[3][18], '5');
 	}
+	
+	@Test
+	public void testLastFrameSecondBowlSpare() 
+	{
+		game1.getList().get(9).setHit1(6);
+		game1.setLastFrameSecondBowl(4, 9);
+		assertEquals(game1.getScoreboard()[2][67], '/');
+	}
+
+	@Test
+	public void testLastFrameSecondBowlSpare2() 
+	{
+		game1.getList().get(9).setHit1(5);
+		game1.setLastFrameSecondBowl(5,9);
+		game1.getList().get(9).setHit2(5);
+		game1.setLastFrameThirdBowl(5,9);
+		assertNotEquals(game1.getScoreboard()[2][68], '/');
+		assertEquals(game1.getScoreboard()[2][68], '5');
+		assertEquals(game1.getScoreboard()[2][67], '/');
+	}
+	@Test
+	public void testLastFrameThirdBowlSpare() 
+	{
+		game1.getList().get(9).setHit1(10);
+		game1.setLastFrameSecondBowl(4,9);
+		game1.getList().get(9).setHit2(4);
+		game1.setLastFrameThirdBowl(6,9);
+		assertNotEquals(game1.getScoreboard()[2][67], '/');
+		assertEquals(game1.getScoreboard()[2][67], '4');
+		assertEquals(game1.getScoreboard()[2][68], '/');
+	}
+	
+	@Test
+	public void manuallyInputHitsInFullGame()
+	{
+		game1.getList().get(0).setHit1(5);
+		game1.getList().get(0).setHit2(5);
+		game1.getList().get(1).setHit1(8);
+		game1.getList().get(1).setHit2(2);
+		game1.getList().get(2).setHit1(9);
+		game1.getList().get(2).setHit2(1);
+		game1.getList().get(3).setHit1(9);
+		game1.getList().get(3).setHit2(0);
+		game1.getList().get(4).setHit1(7);
+		game1.getList().get(4).setHit2(1);
+		game1.getList().get(5).setHit1(8);
+		game1.getList().get(5).setHit2(2);
+		game1.getList().get(6).setHit1(10);
+		game1.getList().get(7).setHit1(3);
+		game1.getList().get(7).setHit2(5);
+		game1.getList().get(8).setHit1(10);
+		game1.getList().get(9).setHit1(5);
+		game1.getList().get(9).setHit2(3);
+		assertEquals(game1.getScore(), 0);	
+	}//////////////////////change//////////////////////
+	
+	@Test
+	public void testReturnPoints()
+	{
+		game1.setHit(5);
+		game1.rollBall();
+		game1.setHit(5);
+		game1.rollBall();
+		
+		game1.setHit(5);
+		game1.rollBall();
+		game1.setHit(5);
+		game1.rollBall();
+		
+		game1.setHit(5);
+		game1.rollBall();
+		game1.setHit(5);
+		game1.rollBall();
+		
+		game1.setHit(5);
+		game1.rollBall();
+		game1.setHit(5);
+		game1.rollBall();
+		
+		game1.setHit(5);
+		game1.rollBall();
+		game1.setHit(5);
+		game1.rollBall();
+		
+		game1.setHit(10);
+		game1.rollBall();
+		
+		game1.setHit(10);
+		game1.rollBall();
+		
+		game1.setHit(5);
+		game1.rollBall();
+		game1.setHit(5);
+		game1.rollBall();
+		
+		game1.setHit(5);
+		game1.rollBall();
+		game1.setHit(5);
+		game1.rollBall();
+		
+		game1.setHit(10);
+		game1.rollBall();
+		game1.setHit(5);
+		game1.rollBall();
+		game1.setHit(5);
+		game1.rollBall();
+		
+		assertEquals(game1.getScore(), 180);	
+//		
+	}
+	
+	
 }
